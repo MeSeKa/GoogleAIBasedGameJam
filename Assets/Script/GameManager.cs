45d6af15f98a0bbc613935b9e1344b7b48097b72
Mehmed Sefa Karataş <mehmedsefakaratas@gmail.com>
2024-05-18 21:07:45 +0300

    Merge branch 'MLAgentWorking' into Erdenay2

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,78 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private TimeState currentState = TimeState.New;
+
+    [SerializeField] FloorChanger floorChanger;
+    [SerializeField] Crane crane;
+
+    private float playerDisablingTime = 1f;
+    private float environmentChangingTime = 1f;
+    private float playerEnablingTime = 1f;
+
+    [SerializeField] GameObject player;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            FullTimeChange();
+        }
+    }
+
+    public void FullTimeChange()
+    {
+        ChangeState();
+
+        DisablePlayer();
+        Invoke("ChangeEnvironment", playerDisablingTime);
+        Invoke("EnablePlayer", playerDisablingTime + environmentChangingTime);
+        Invoke("Finish", playerDisablingTime + environmentChangingTime + playerEnablingTime);
+
+    }
+
+    void ChangeState()
+    {
+        switch (currentState)
+        {
+            case TimeState.Old:
+                currentState = TimeState.New;
+                break;
+            case TimeState.New:
+                currentState = TimeState.Old;
+                break;
+
+        }
+    }
+
+    void DisablePlayer()
+    {
+        crane.Show();
+        crane.TakePlayer();
+        player.GetComponent<ThirdPersonController>().enabled = false;
+        player.GetComponent<CharacterController>().enabled = true;
+    }
+
+    void ChangeEnvironment()
+    {
+        //crane.DeShow();
+        floorChanger.ChangeNewState(currentState);
+    }
+
+    void EnablePlayer()
+    {
+        //crane.Show();
+        crane.ReleasePlayer();
+    }
+
+    void Finish()
+    {
+        crane.DeShow();
+        player.GetComponent<ThirdPersonController>().enabled = true;
+        player.GetComponent<CharacterController>().enabled = true;
+    }
+}
